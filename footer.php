d126dd4ddf47536eb4aad2181e74f32f7ea6fcab
trixiedeea <38724876+trixiedeea@users.noreply.github.com>
2026-07-11 01:34:17 -0700

    commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+       <footer>
+        <p>&copy; <?php echo date("Y"); ?> Traci Dirom Info 240 Final Project. All rights reserved.</p>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tallahassee</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li class="navList"><a href="#gameOverview">Game Overview</a></li>
+                <li class="navList"><a href="#boardDesign">Board Design</a>
+                    <ul class="paths">
+                        <li><a href="#expansion">Age Of Expansion</a></li>
+                    </ul>
+                </li>
+                <li class="navList"><a href="#gameComponents">Game Components</a></li>
+                <li class="navList"><a href="#playerRoles">Player Roles</a></li>
+            </ul>
+        </nav>
+    </header>
+
