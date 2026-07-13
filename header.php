918859642d25e7eefbe27513e6cc4a3c07c5526c
trixiedeea <38724876+trixiedeea@users.noreply.github.com>
2026-07-13 15:45:08 -0700

    commit

--- header.php
+++ header.php
@@ -1,36 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE php>
+<php lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Tallahassee</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="startScreen">
     <header>
         <nav>
             <ul class="navbar">
-                <li class="navList"><a href="#home">Home</a></li>
-                <li class="navList"><a href="#history">History</a>
+                <li class="navList"><a href="index.php">Home</a>
+                    <ul class="homeList">
+                        <li><a href="index.php#intro">Welcome To Tallahassee</a></li>
+                        <li><a href="index.php#government">Government</a></li>
+                        <li><a href="index.php#site">This Site</a></li>
+                        <li><a href="index.php#form">Contact Us Form</a></li>
+                    </ul>
+                </li>
+                <li class="navList"><a href="historyIndex.php">History</a>
                     <ul class="historyList">
-                        <li><a href="#founding">Founding of Tallahassee</a></li>
-                        <li><a href="#thirties">1830s</a></li>
-                        <li><a href="#forties">1840s-1860s</a></li>
-                        <li><a href="#reconstruction">Reconstruction, 1880s and 1890s</a></li>
+                        <li><a href="historyIndex.php#founding">Founding of Tallahassee</a></li>
+                        <li><a href="historyIndex.php#thirties">1830s</a></li>
+                        <li><a href="historyIndex.php#forties">1840s-1860s</a></li>
                     </ul>
                 </li>
-                <li class="navList"><a href="#politics">Politics</a>
+                <li class="navList"><a href="politicsIndex.php">Politics</a>
                     <ul class ="politicsList">
-                        <li><a href="politicalCenter">Florida's Political Center</a></li>
-                        <li><a href="policy">Civic Engagement and Public Policy</a></li>
-                        <li><a href="future">The Future of Politics in Tallahassee</a></li>
+                        <li><a href="politicsIndex.php#politicalCenter">Florida's Political Center</a></li>
+                        <li><a href="politicsIndex.php#policy">Civic Engagement and Public Policy</a></li>
+                        <li><a href="politicsIndex.php#future">The Future of Politics in Tallahassee</a></li>
                     </ul>
                 </li>
-                <li class="navList"><a href="#zombieland">Zombieland</a>
+                <li class="navList"><a href="zombielandIndex.php">Zombieland</a>
                     <ul class="zombielandList">
-                        <li><a href="#appearence">Appearence, Personality and Quirks</a></li>
-                        <li><a href="#preZombieland">Pre-Zombieland And Zombieland</a></li>
-                        <li><a href="#doubletap">Doubletap</a></li>
-                        <li><a href="#albuquerque">Albuquerque</a></li>
+                        <li><a href="zombielandIndex.php#appearance">Appearence, Personality and Quirks</a></li>
+                        <li><a href="zombielandIndex.php#prezombieland">Pre-Zombieland And Zombieland</a></li>
+                        <li><a href="zombielandIndex.php#doubletap">Doubletap</a></li>
                     </ul>
                 </li>
             </ul>
