cc9bd8b79b429ed13d945bdc525eea8c0738fa40
trixiedeea <38724876+trixiedeea@users.noreply.github.com>
2026-07-12 20:52:01 -0700

    commit

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
-    <footer> <a href="#">Privacy Policy</a> | <a href="#">Contact</a></p>
-        <br><br><br>
-        <p >Copyright &copy; - All rights reserved Traci Dirom Info 240 Final Project</p>
+    <footer><br><br><br>
+        <p >Copyright &copy; 2026 - All rights reserved Traci Dirom Info 240 Final Project</p>
     </footer>
 </body>
 </html>
