fd716957684c25b4a69fe505feb69ed55a51a62a
trixiedeea <38724876+trixiedeea@users.noreply.github.com>
2026-07-13 17:56:39 -0700

    commit

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer class="footer-text"><br><br><br>
         <span class="left-text">Copyright &copy; 2026 - All rights reserved Traci Dirom Info 240 Final Project</span>
-        <span class="right-text"><a href="index.html#form">Contact Us</a> |<a href="Tallahassee.txt">Privacy Policy</a></span>
+        <span class="right-text"><a href="index.html#form">Contact Us</a> |<a href="PrivacyPolicy.txt">Privacy Policy</a></span>
     </footer>
 </div>
 </body>
